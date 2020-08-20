aa69726646575a4125f6da86f7b36df482749db1
Nikhil Pratap Singh <nikhilpsrajput1234@gmail.com>
2020-08-20 16:56:32 +0530

    hello

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#include "v2.0.0 helpers.h"
+
+int main(void)
+{
+    int n = 4;
+    printf("n: ");
+    scanf("%d", &n);
+    printf("Fibonacci of %d = %d", n, fibOfN(n));
+    printf("again hello world");
+    printf("Something more by vkweb");
+    printf("Mai Anish hoon")//this line is added
+    return 0;
+}
+// fib(6) = 8
+//
